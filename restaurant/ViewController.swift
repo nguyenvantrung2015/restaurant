8f79510fcaa8dbdc88446a9cbdcabe23c04d83a5
trungnv <nguyenvantrung2016@gmail.com>
2019-08-20 20:06:00 +0700

    init project

---
+++ restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  restaurant
+//
+//  Created by Trung on 8/20/19.
+//  Copyright © 2019 Jibun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
